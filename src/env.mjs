044cd2d48ce02b4bf84c8e46e4f6b3b4fe23be4d
Gaurav kumar yadav <97657189+gauravyad12@users.noreply.github.com>
2024-10-24 20:24:25 +0530

    Update env.mjs

--- src/env.mjs
+++ src/env.mjs
@@ -38,13 +38,13 @@ export const env = createEnv({
     NEXT_PUBLIC_GOOGLE_ANALYTICS_ID:
       process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID,
     NEXT_PUBLIC_SITE_NAME: process.env.NEXT_PUBLIC_SITE_NAME,
-    NEXT_PUBLIC_TWITTER: process.env.NEXT_PUBLIC_TWITTER ?? 'https://x.com',
+    NEXT_PUBLIC_TWITTER: process.env.NEXT_PUBLIC_TWITTER ?? 'https://x.com/DarkDev086',
     NEXT_PUBLIC_FACEBOOK:
-      process.env.NEXT_PUBLIC_FACEBOOK ?? 'https://facebook.com',
+      process.env.NEXT_PUBLIC_FACEBOOK ?? 'https://facebook.com/Darkdev064',
     NEXT_PUBLIC_INSTAGRAM:
-      process.env.NEXT_PUBLIC_INSTAGRAM ?? 'https://instagram.com',
+      process.env.NEXT_PUBLIC_INSTAGRAM ?? 'https://instagram.com/youhaveme064/',
     NEXT_PUBLIC_YOUTUBE:
-      process.env.NEXT_PUBLIC_YOUTUBE ?? 'https://youtube.com',
+      process.env.NEXT_PUBLIC_YOUTUBE ?? 'https://youtube.com/@flyingdarkdev',
     NEXT_PUBLIC_IMAGE_DOMAIN: process.env.NEXT_PUBLIC_IMAGE_DOMAIN,
   },
   /**
